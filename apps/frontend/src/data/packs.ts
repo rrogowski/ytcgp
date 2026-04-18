1d92955cf80a1ca8d84d0542174f6047fc96e9db
Roman Rogowski <roman.j.rogowski@gmail.com>
2026-04-18 15:27:10 -0400

    feat: packs skeleton

---
+++ apps/frontend/src/data/packs.ts
@@ -0,0 +1,8 @@
+export const ALL_PACKS = [
+  {
+    code: "LOB",
+    name: "Legend of Blue Eyes White Dragon",
+    imageUrl:
+      "https://static.wikia.nocookie.net/yugioh/images/9/9f/LOB-BoosterNA.jpg",
+  },
+];
--- apps/frontend/src/screens/packs.tsx
+++ apps/frontend/src/screens/packs.tsx
@@ -1,3 +1,19 @@
+import { Fragment } from "react";
+import { ALL_PACKS } from "../data/packs";
+
 export const Packs: React.FC = () => {
-  return <>Packs</>;
+  return (
+    <>
+      {ALL_PACKS.map((pack) => {
+        return (
+          <Fragment key={pack.code}>
+            {pack.name}
+            <img src={pack.imageUrl}></img>
+            <button>Buy</button>
+            <button>Craft</button>
+          </Fragment>
+        );
+      })}
+    </>
+  );
 };
