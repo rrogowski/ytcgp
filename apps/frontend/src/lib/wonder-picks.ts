26fd6f065534234e636153e38e0ee5279b0f9943
Roman Rogowski <roman.j.rogowski@gmail.com>
2026-04-29 09:15:32 -0400

    fix: wonder packs exclude user's own packs

--- apps/frontend/src/lib/wonder-picks.ts
+++ apps/frontend/src/lib/wonder-picks.ts
@@ -2,26 +2,32 @@ import { limit, orderBy } from "firebase/firestore";
 import { useMemo } from "react";
 import { packsRef } from "../models/pack";
 import { useProfile } from "../models/profile";
+import { useUser } from "./auth";
 import { useCollection } from "./firestore";
 
 const RECENT_PACKS_CONSTRAINTS = [orderBy("createdAt", "desc"), limit(20)];
 
 export const useWonderPicks = () => {
+  const user = useUser();
   const profile = useProfile();
   const packs = useCollection(packsRef, RECENT_PACKS_CONSTRAINTS);
 
+  const wonderPacks = useMemo(() => {
+    return packs.docs.filter((p) => p.data.userUid !== user.uid);
+  }, [packs.docs, user.uid]);
+
   const hasNewPicks = useMemo(() => {
-    if (!profile.data || packs.docs.length === 0) {
+    if (!profile.data || wonderPacks.length === 0) {
       return false;
     }
     if (!profile.data.lastViewedWonderPicksAt) {
       return true;
     }
     return (
-      packs.docs[0].data.createdAt.toMillis() >=
+      wonderPacks[0].data.createdAt.toMillis() >=
       profile.data.lastViewedWonderPicksAt.toMillis()
     );
-  }, [profile.data, packs.docs]);
+  }, [profile.data, wonderPacks]);
 
-  return { hasNewPicks };
+  return { hasNewPicks, wonderPacks };
 };
