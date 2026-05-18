9b4238fda32d5e3d85bb5c93514e83619f81f5bd
Roman Rogowski <roman.j.rogowski@gmail.com>
2026-05-18 08:06:04 -0400

    feat: release expansion

--- apps/frontend/src/data/expansions.ts
+++ apps/frontend/src/data/expansions.ts
@@ -1,11 +1,7 @@
 import { findPackByCode } from "./packs";
 
-const now = new Date();
-
 export const ALL_EXPANSIONS = [
-  ...(now.getTime() >= Date.parse("May 18, 2026") && now.getHours() >= 8
-    ? [{ name: "Mechanical Dominion", codes: ["MRD", "TP1", "SRL"] }]
-    : []),
+  { name: "Mechanical Dominion", codes: ["MRD", "TP1", "SRL"] },
   { name: "Rivals Clash", codes: ["SDY", "SDK", "DDS"] },
   { name: "Genesis", codes: ["LOB", "MP1"] },
 ];
