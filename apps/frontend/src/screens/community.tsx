ae2c1830e4fe04e7c4be611708fa6ce36180bc13
Roman Rogowski <roman.j.rogowski@gmail.com>
2026-05-18 13:55:08 -0400

    revert: undo center align preview

--- apps/frontend/src/screens/community.tsx
+++ apps/frontend/src/screens/community.tsx
@@ -64,7 +64,6 @@ export const Community: React.FC = () => {
   return (
     <div
       style={{
-        alignItems: "center",
         display: "flex",
         flexDirection: "column",
         height: "100%",
