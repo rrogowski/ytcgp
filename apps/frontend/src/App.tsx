b728c65d265f4e5f729247f6fecc31748d509f50
Roman Rogowski <roman.j.rogowski@gmail.com>
2026-08-08 16:44:45 -0400

    refactor: relocate navbar

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,10 +1,8 @@
-import "./App.css";
 import "./styles.css";
 
 import { FullPageText } from "./components/full-page-text";
-import { Icon } from "./components/icon";
+import { Navbar } from "./components/navbar";
 import { Button } from "./design-system/components/button";
-import { Column } from "./design-system/components/column";
 import { Container } from "./design-system/components/container";
 import { Expanded } from "./design-system/components/expanded";
 import { Image } from "./design-system/components/image";
@@ -18,7 +16,6 @@ import { useAuth } from "./lib/auth";
 import { PENDING_ID, useDocumentWithId } from "./lib/firestore";
 import { useRouter } from "./lib/router";
 import { useTransaction } from "./lib/transaction";
-import { useWonderPicks } from "./lib/wonder-picks";
 import { pointsWalletsRef } from "./models/points-wallet";
 import { useProfile } from "./models/profile";
 import { Binder } from "./screens/binder";
@@ -29,13 +26,6 @@ import { Packs } from "./screens/packs";
 import { Search } from "./screens/search";
 import { WonderPick } from "./screens/wonder-pick";
 import { claimAllowanceTransaction } from "./transactions/allowance";
-
-const NAVBAR_BUTTONS = [
-  { icon: "book_5", path: "/binder", aliases: ["/", "/search"] },
-  { icon: "store", path: "/packs", aliases: ["/pack", "/craft"] },
-  { icon: "diversity_3", path: "/community" },
-  { icon: "select_all", path: "/wonder-pick" },
-];
 
 const App: React.FC = () => {
   const allowance = useAllowance();
@@ -93,61 +83,6 @@ const App: React.FC = () => {
       </Expanded>
       <Navbar type="mobile"></Navbar>
     </SafeArea>
-  );
-};
-
-const Navbar: React.FC<{ type: "desktop" | "mobile" }> = (props) => {
-  const auth = useAuth();
-  const router = useRouter();
-  const wonderPicks = useWonderPicks();
-  const Component = props.type === "desktop" ? Column : Row;
-
-  return (
-    <Component
-      backgroundColor="#7d5646"
-      className={`Navbar ${props.type}`}
-      gap="0.5rem"
-      justifyContent={props.type === "mobile" ? "space-around" : ""}
-      padding={props.type === "desktop" ? "1rem 0.5rem" : "0.5rem"}
-    >
-      {NAVBAR_BUTTONS.map(({ icon, path, aliases = [] }) => {
-        const isActive = [path, ...aliases].includes(router.path);
-        return (
-          <Button
-            key={path}
-            alignItems="center"
-            aspectRatio="1 / 1"
-            backgroundColor={isActive ? "white" : "inherit"}
-            borderRadius="0.75rem"
-            color={isActive ? "inherit" : "white"}
-            disabled={auth.user === null}
-            display="flex"
-            fontSize="1.5rem"
-            justifyContent="center"
-            position="relative"
-            width="2.375rem"
-            onClick={() => router.navigate(path)}
-          >
-            <Icon>{icon}</Icon>
-            {path === "/wonder-pick" && wonderPicks.hasNewPicks && (
-              <Text
-                backgroundColor="red"
-                borderRadius="0.25rem"
-                color="white"
-                fontSize="0.6rem"
-                padding="0.25rem"
-                pointerEvents="none"
-                position="absolute"
-                right="-0.5rem"
-                top="-0.5rem"
-              >
-                New
-              </Text>
-            )}
-          </Button>
-        );
-      })}
-    </Component>
   );
 };
 
---
+++ apps/frontend/src/components/navbar.tsx
@@ -0,0 +1,72 @@
+import "./navbar.css";
+
+import { Icon } from "../components/icon";
+import { Button } from "../design-system/components/button";
+import { Column } from "../design-system/components/column";
+import { Row } from "../design-system/components/row";
+import { Text } from "../design-system/components/text";
+import { useAuth } from "../lib/auth";
+import { useRouter } from "../lib/router";
+import { useWonderPicks } from "../lib/wonder-picks";
+
+const NAVBAR_BUTTONS = [
+  { icon: "book_5", path: "/binder", aliases: ["/", "/search"] },
+  { icon: "store", path: "/packs", aliases: ["/pack", "/craft"] },
+  { icon: "diversity_3", path: "/community" },
+  { icon: "select_all", path: "/wonder-pick" },
+];
+
+export const Navbar: React.FC<{ type: "desktop" | "mobile" }> = (props) => {
+  const auth = useAuth();
+  const router = useRouter();
+  const wonderPicks = useWonderPicks();
+  const Component = props.type === "desktop" ? Column : Row;
+
+  return (
+    <Component
+      backgroundColor="#7d5646"
+      className={`Navbar ${props.type}`}
+      gap="0.5rem"
+      justifyContent={props.type === "mobile" ? "space-around" : ""}
+      padding={props.type === "desktop" ? "1rem 0.5rem" : "0.5rem"}
+    >
+      {NAVBAR_BUTTONS.map(({ icon, path, aliases = [] }) => {
+        const isActive = [path, ...aliases].includes(router.path);
+        return (
+          <Button
+            key={path}
+            alignItems="center"
+            aspectRatio="1 / 1"
+            backgroundColor={isActive ? "white" : "inherit"}
+            borderRadius="0.75rem"
+            color={isActive ? "inherit" : "white"}
+            disabled={auth.user === null}
+            display="flex"
+            fontSize="1.5rem"
+            justifyContent="center"
+            position="relative"
+            width="2.375rem"
+            onClick={() => router.navigate(path)}
+          >
+            <Icon>{icon}</Icon>
+            {path === "/wonder-pick" && wonderPicks.hasNewPicks && (
+              <Text
+                backgroundColor="red"
+                borderRadius="0.25rem"
+                color="white"
+                fontSize="0.6rem"
+                padding="0.25rem"
+                pointerEvents="none"
+                position="absolute"
+                right="-0.5rem"
+                top="-0.5rem"
+              >
+                New
+              </Text>
+            )}
+          </Button>
+        );
+      })}
+    </Component>
+  );
+};
